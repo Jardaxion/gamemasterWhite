3b1eb115ce83af9e646e1e92315120a5ec26b0a4
Semyon <bainthecreator21@gmail.com>
2023-03-09 01:54:29 +0700

    123

--- js/main.js
+++ js/main.js
@@ -198,6 +198,21 @@ $(document).ready(function() {
 
         reOpenModal($(this).data('modal'));
     })
+
+    if (/iPhone/.test(navigator.userAgent) && !window.MSStream)
+    {
+        $(document).on("focus", "input, textarea, select", function()
+        {
+            $('meta[name=viewport]').remove();
+            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">');
+        });
+
+        $(document).on("blur", "input, textarea, select", function()
+        {
+            $('meta[name=viewport]').remove();
+            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+        });
+    }
 })
 
 //Модальные окна
