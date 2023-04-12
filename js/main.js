4059ae5d9fc3462f3ef6cd48ec4890a598f1baed
Jardaxion <bainthecreator21@gmail.com>
2023-04-13 03:13:24 +0700

    312

--- js/main.js
+++ js/main.js
@@ -486,20 +486,20 @@ $(document).ready(function() {
         console.log(e.target);
     })
 
-    if (/iPhone/.test(navigator.userAgent) && !window.MSStream)
-    {
-        $(document).on("focus", "input, textarea, select", function()
-        {
-            $('meta[name=viewport]').remove();
-            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">');
-        });
+    // if (/iPhone/.test(navigator.userAgent) && !window.MSStream)
+    // {
+    //     $(document).on("focus", "input, textarea, select", function()
+    //     {
+    //         $('meta[name=viewport]').remove();
+    //         $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">');
+    //     });
 
-        $(document).on("blur", "input, textarea, select", function()
-        {
-            $('meta[name=viewport]').remove();
-            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
-        });
-    }
+    //     $(document).on("blur", "input, textarea, select", function()
+    //     {
+    //         $('meta[name=viewport]').remove();
+    //         $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+    //     });
+    // }
 
     document.querySelectorAll('.lineSlider').forEach(function(el){
         new Swiper(el, {
